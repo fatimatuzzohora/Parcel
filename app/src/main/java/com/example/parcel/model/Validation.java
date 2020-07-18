b824c6cd7d4ba79299ec2d68cf7d70db4fbc5649
shamim <tuzzohora.fatima@gmail.com>
2020-07-18 11:13:18 +0600

    Review7

--- app/src/main/java/com/example/parcel/model/Validation.java
+++ app/src/main/java/com/example/parcel/model/Validation.java
@@ -2,6 +2,8 @@ package com.example.parcel.model;
 
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 
 import com.example.parcel.R;
@@ -11,12 +13,14 @@ public class Validation {
     public static CharSequence hintName;
 
     //Edittext validation
-    public static boolean editTextValidation(EditText editText, String errorMssage){
+    public static boolean editTextValidation(final EditText editText, String errorMssage){
         if(!editText.getText().toString().isEmpty()){
+            editText.clearFocus();
+
             return true;
         }
         else {
-            editText.setError(errorMssage);
+            //editText.setError(errorMssage);
 
             editText.requestFocus();
             hintName = editText.getHint();
